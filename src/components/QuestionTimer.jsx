cb1674d43731d3e86029bcfac4d24f7cd69695bb
Ida Ganji <ida.ganji@omegapoint.no>
2025-02-17 18:50:10 +0100

    The Key prop in react

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -2,17 +2,17 @@ import { useState, useEffect } from "react"
 export default function QuestionTimer({ timeout, onTimeOut }) {
     const [remainingTime, setRemainingTime] = useState(timeout)
     useEffect(() => {
-        setTimeout(onTimeOut, timeout)
+       const timeOut= setTimeout(onTimeOut, timeout)
         console.log("Setting Timeout"); 
+        return () => {
+            clearTimeout(timeOut)
+        }
     }, [onTimeOut, timeout])
-    //The cleanup function in useEffect runs before the component unmounts or before the effect re-runs.
     useEffect(() => {
      const interval= setInterval(() => {
             setRemainingTime(prev => prev - 100)
         }, 100)
         console.log("Setting Interval");
-        // in our case:
-        // this function gets automatically called by react before the component is unmounted from the dom 
         return () => {
             clearInterval(interval)
         }
@@ -21,13 +21,5 @@ export default function QuestionTimer({ timeout, onTimeOut }) {
         <p>{ remainingTime}</p>
     <progress id="question-time" max={timeout} value={remainingTime}></progress></>
 }
-// The issue of the last commit was that the timer depleated at half of the time defined in timeOut
-// reason is that even though we are wrapping setInterval with useEffect, the code gets called twice!
-// Due to strictMode in development
-//The components get umounted and mounted again so the UseEffects run twice!
-//If the useEffect runs twice (due to Strict Mode, hot reload, or component re-mounting), two separate setInterval instances are created.
-//Each interval decreases remainingTime every 100 ms independently, meaning the state is updated twice as often.
-//Instead of decrementing remainingTime by 100 ms every 100 ms, it now decrements 200 ms every 100 ms, effectively making the timer run twice as fast.
-//::Solution:://
-// We need a cleanup function in usEeffect
+
 
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -5,19 +5,12 @@ import QuestionTimer from "./QuestionTimer.jsx"
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([])
     const activeQuestionIndex = userAnswers.length
-    // first make a copy of answers because sort mutates the array
     const quizIsOver = activeQuestionIndex === QUESTINOS.length
-    // now handleSelectAnswer should be wrapped by useCallBack
-    // no dependencies because the only thing used is state-updating function and that is not needed as a dependency
     const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
         setUserAnswers((prev) => {
             return [...prev, selectedAnswer]
         })
     }, []);
-    // first the arrow-function
-    // we wrap it with useCallBack and pass the handleselectanswer as dependency
-    // because that function is a value that internally depends on props and states
-
     const handleTimerExpire = useCallback(() => {
         handleSelectAnswer(null)
     }, [handleSelectAnswer]); 
@@ -29,14 +22,11 @@ export default function Quiz() {
             <h2>Quiz completed!</h2>
         </div>
     }
-    // Now the last round you end up in the quizIsOver code-block and after return no code is executed
     const copyAnswers= [...QUESTINOS[activeQuestionIndex].answers]
     const shuffledAnswers = copyAnswers.sort(() => Math.random() - 0.5)
-    // make sure to always pass a reference to the function and not immediatelycall the function in JSX
-    // you can do it by passing an arrow function that calls the original event-listener
     return (<div id="quiz">
         <div id="question">
-            <QuestionTimer timeout={10000} onTimeOut={handleTimerExpire}/>
+            <QuestionTimer timeout={10000} onTimeOut={handleTimerExpire} key={activeQuestionIndex}/>
             <h2>{QUESTINOS[activeQuestionIndex].text}</h2>
             <ul id="answers">
                 {shuffledAnswers.map((answer, i) =>
@@ -47,10 +37,9 @@ export default function Quiz() {
         </div>
     </div>);
 }
-// The solution to the previous commit is to wrap the functions with useCallback hook
-// because everytime this component is run again, the handleSelectAnswer and the
-// nameless arrow-function () =>{handleSelectAnswer(null)} are brand new functions in memory.
-// With the useCallBack in place, our functions are not re-executed just because the surrounding componet has re-rendered
-
-///::Issue::://
-// we still have the wired pause after the timer has ran out and the next question being rendered!
+// Now the issue is that when we are finised with the first question and go to the next, this component is re-rendered
+// But since the QuestionTimer component will not re-render as it has not changed
+// But since we want the timers to be re-set, we need to make react un-mount and re-mount this component for us
+// Solution::: key-prop
+// All the components in react have a special Key prop 
+// This prop when its value changes -> tells React to destroy the old component and create a new one/ umount and remount the component
