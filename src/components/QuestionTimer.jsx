59e861e80e7f8c7a8335d125dae906187f631ff6
Ida Ganji <ida.ganji@omegapoint.no>
2025-02-17 18:36:31 +0100

    Use Effect CleanUp function

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,20 +1,33 @@
 import { useState, useEffect } from "react"
 export default function QuestionTimer({ timeout, onTimeOut }) {
     const [remainingTime, setRemainingTime] = useState(timeout)
-    //If we don´t wrap it with useEffect, on every re-render so runs this code and we get a new timeOut
-    // We only want one timeout and it should be run again if any of the props/states passed to it change
-    // For example if the parent component changes the timeout or the onTimeout function then we want setTimeout to be called again with the updated properties
     useEffect(() => {
         setTimeout(onTimeOut, timeout)
         console.log("Setting Timeout"); 
     }, [onTimeOut, timeout])
-    // we want our remainingTime state to gets updated every 100ms, but we only want setInterval to happen once
-    // so we need to wrap it with useeffect because otherwise we would have this evig loop 
+    //The cleanup function in useEffect runs before the component unmounts or before the effect re-runs.
     useEffect(() => {
-        setInterval(() => {
+     const interval= setInterval(() => {
             setRemainingTime(prev => prev - 100)
         }, 100)
         console.log("Setting Interval");
+        // in our case:
+        // this function gets automatically called by react before the component is unmounted from the dom 
+        return () => {
+            clearInterval(interval)
+        }
     },[])
-    return <progress id="question-time" max={timeout} value={remainingTime}></progress>
+    return <>
+        <p>{ remainingTime}</p>
+    <progress id="question-time" max={timeout} value={remainingTime}></progress></>
 }
+// The issue of the last commit was that the timer depleated at half of the time defined in timeOut
+// reason is that even though we are wrapping setInterval with useEffect, the code gets called twice!
+// Due to strictMode in development
+//The components get umounted and mounted again so the UseEffects run twice!
+//If the useEffect runs twice (due to Strict Mode, hot reload, or component re-mounting), two separate setInterval instances are created.
+//Each interval decreases remainingTime every 100 ms independently, meaning the state is updated twice as often.
+//Instead of decrementing remainingTime by 100 ms every 100 ms, it now decrements 200 ms every 100 ms, effectively making the timer run twice as fast.
+//::Solution:://
+// We need a cleanup function in usEeffect
+
