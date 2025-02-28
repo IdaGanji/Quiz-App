fde46f6610de4b365606e5a24cf8d64efb010f8d
Ida Ganji <ida.ganji@omegapoint.no>
2025-02-28 12:56:56 +0100

    Change the variable to be a ref-variable to persist its value across re-renderings. Faced a new issue

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,9 @@
-import { useState, useCallback } from "react"
+import { useState, useCallback, useRef } from "react"
 import QUESTINOS from '../../questions.js'
 import Over from '../assets/quiz-complete.png'
 import QuestionTimer from "./QuestionTimer.jsx"
 export default function Quiz() {
+    const shuffledAnswers = useRef();
     const [userAnswers, setUserAnswers] = useState([])
     const [answerState, setAnswerState] = useState('');
     // We added more logic here
@@ -40,14 +41,25 @@ export default function Quiz() {
             <h2>Quiz completed!</h2>
         </div>
     }
-    const copyAnswers= [...QUESTINOS[activeQuestionIndex].answers]
-    const shuffledAnswers = copyAnswers.sort(() => Math.random() - 0.5)
+    // Now the bug in the last commit was that since we added another state (answerState)
+    // Then the UI re-renders immediately at line 13 when the user chooses an answer
+    // So the shuffledAnswers variable is calculated again and since the question index has not changed yet
+    // We don´t immediately jump to the next question and but the same questions answers gets shuffled
+    ///::::::Solution::::
+    // we can store the value of the shuffledAnswers in a ref-variable
+    // Since ref-variables persist their value independently of component being re-rendered then the value won´t be re-calculated
+    // at every component re-render
+    // So we can check if the current is undefined, we sort it otherwise not.
+    if (!shuffledAnswers.current) {
+        shuffledAnswers.current = [...QUESTINOS[activeQuestionIndex].answers]
+        shuffledAnswers.current.sort(() => Math.random() - 0.5);
+    }
     return (<div id="quiz">
         <div id="question">
             <QuestionTimer timeout={10000} onTimeOut={handleTimerExpire} key={activeQuestionIndex}/>
             <h2>{QUESTINOS[activeQuestionIndex].text}</h2>
             <ul id="answers">
-                {shuffledAnswers.map((answer, i) => {
+                {shuffledAnswers.current.map((answer, i) => {
                     const isSelected = userAnswers[userAnswers.length - 1] === answer;
                     let cssClass = '';
                     if (answerState === 'answered' && isSelected) {
